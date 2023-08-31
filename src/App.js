87026a33a1f5cab65c3125127d0c28bdccbfd01f
kang <breaker0105@nate.com>
2023-08-31 10:51:22 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
+  
+  const [hedd, setHedd] =useState([
+    {
+      id:1, memo: 'react를 배워봅시다',
+    }
+  ])
+
+
+  const [txt, setTxt] = useState('')
+
+  const mouse = (e) => {
+    setTxt(e.target.value)
+  }
+  const musebtn = () => {
+     // e.preventDefault();
+  const newobj = {
+    id:hedd.length+1,
+    memo:txt,
+  }
+  setHedd([...hedd, newobj])
+  setTxt('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="icon">
+        
+        <input type="text" 
+        value={txt}
+        onChange={mouse}
+        />
+        
+        <button onClick={musebtn}>추가하기</button>
+       
+      </div>
+      <h2 className="ha">Todo List</h2>
+      <div className="whh">
+       
+            {hedd.map((item) => (
+             
+              <div key={item.id}>
+               <div className = "box">{item.memo}</div>
+              </div>
+              
+            ))}
+       
+      </div>
+      
+        
     </div>
   );
 }
